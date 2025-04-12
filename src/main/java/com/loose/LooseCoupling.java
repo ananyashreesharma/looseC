fbc60dda19143e84783341a4e4a83fa01bc6f051
anannyashreesharma <ananyashreesharma19@gmail.com>
2025-04-12 23:46:30 +0530

    loose coupling cont...

--- src/main/java/com/loose/LooseCoupling.java
+++ src/main/java/com/loose/LooseCoupling.java
@@ -4,7 +4,6 @@ import com.tight.UserManager;
 
 public class LooseCoupling {
     public static void main(String args[]){
-         UserManager userM=new UserManager();
-        System.out.println(userM.getDetails());
+         UserDataProvider databaseProvider = new UserDatabaseProvider();
     }
 }
